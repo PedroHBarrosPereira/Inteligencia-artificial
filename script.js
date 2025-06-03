148d57dbc13a70efa65524b1b03154320f042b6d
PedroHBarrosPereira <143132875+PedroHBarrosPereira@users.noreply.github.com>
2025-06-03 12:37:20 -0300

    .

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativa = document.querySelector(".caixa-alternativa")
 const caixaResultado = document.querySelector(".caixa-resultado")
 const textoResultado = document.querySelector(".texto-resultado")
+
+const perguntas = [
+    {
+        
+    }
+]
